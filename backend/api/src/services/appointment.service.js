8346c67ccc2c27fd1caf015420fdadb32c6824d2
Christopher John <picardochristopherjohnoleo1@gmail.com>
2026-04-06 19:50:35 +0800

    fix the missing import for clinic config

--- backend/api/src/services/appointment.service.js
+++ backend/api/src/services/appointment.service.js
@@ -14,6 +14,7 @@ import {
     SERVICE_TIER,
     APPROVAL_STATUS,
     APPOINTMENT_SOURCE,
+    CLINIC_CONFIG,
 } from '../utils/constants.js';
 import { getTodayPH } from '../utils/timezone.js';
 import { addMinutesToTime } from '../utils/time.js';
